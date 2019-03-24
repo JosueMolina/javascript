ec2ec88e21a5470cdb20b66af17ac2934bae9fad
Josue Molina <eusojkevin@gmail.com>
2019-03-24 16:54:22 -0600

    Removing api key

--- react/react-for-beginners-wesbos/src/base.js
+++ react/react-for-beginners-wesbos/src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCCoEa4jBUcri-O_FyzR4VxZrxRjEzUCx0",
+  apiKey: "apiKey",
   authDomain: "catch-of-the-day-kevin-77d74.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-kevin-77d74.firebaseio.com"
 });
